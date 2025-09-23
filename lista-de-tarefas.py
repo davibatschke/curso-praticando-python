ff35d4cf696fa00e9e14cbfb85c36ce12dd84831
davibatschke <davibatschkedev@gmail.com>
2025-09-23 12:40:28 -0300

    Lista de Tarefas de Terminal

---
+++ lista-de-tarefas.py
@@ -0,0 +1,19 @@
+lista_de_tarefas = []
+import os
+
+def menu_principal():
+    os.system('cls')
+    print('Bem-Vindo a sua Lista de Tarefas de Terminal')
+    print('--------------------------------------------')
+
+    opcoes_menu = {'1 - Adicionar/Remover Tarefa': ...,
+                   '2 - Exibir a Lista de Tarefa': ...,
+                   '3 - Alternar Status de Tarefa': ...,
+                   '4 - Encerrar o Programa': ...}
+    for i in opcoes_menu:
+        print(opcoes_menu[i])
+    escolha = input('\nQual das Opções Deseja? ')
+    # continuar montando o menu principal, colocar as opcoes, etc.
+
+
+menu_principal()
